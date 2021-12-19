a5929ea235629d84c7e8d7a03a03a0e06a6094e9
Iván Portillo Pérez <iportillo@MacBook-Air-de-Ivan.local>
2021-12-19 08:59:23 +0100

    modificado maquetación card

---
+++ resources/views/components/card-post.blade.php
@@ -0,0 +1,28 @@
+<!-- component -->
+@props(['post'])
+
+<div
+    class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
+    <div class="w-full md:w-1/3 bg-white grid place-items-center">
+        <img src="{{ Storage::url($post->image->url) }}" alt="{{ $post->name }}" title="{{ $post->name }} }}"
+            class="rounded-xl" />
+    </div>
+    <div class="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+        <div class="flex justify-between item-center gap-2">
+            <p>
+                <i class="fas fa-tags"></i>
+                @foreach ($post->tags as $tag)
+                    <a href="{{ route('tag', $tag->slug) }}">
+                        {{ $tag->name }}
+                    </a>
+                @endforeach
+            </p>
+        </div>
+        <h3 class="font-black text-gray-800 md:text-3xl text-xl">{{ $post->name }}</h3>
+        <p class="md:text-lg text-gray-500 text-base"> {!! Str::limit($post->excerpt, 150, '...') !!}</p>
+        <p class="text-xl font-black text-gray-800">
+            fecha:
+            <span class="font-normal text-gray-600 text-base">{{ $post->created_at->format('d/m/Y') }}</span>
+        </p>
+    </div>
+</div>
--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -3,23 +3,23 @@
         <p class="text-muted text-center small p-0 mb-4">&copy; 2021 Notas Web. Todos los derechos
             reservados. | <a href="{{ route('contact') }}"> Contacto</a> </p>
         <div class="flex justify-center">
-            <div class="col-lg-2 mr-2 text-center">
+            <div class="col-lg-2 mr-1 text-center">
                 <a class="d-block" href="https://www.linkedin.com/in/iván-portillo-pérez-a9676878"
                     target="_blank">
                     <i class="fab fa-linkedin-in fa-2x mb-2 text-muted"></i>
                 </a>
             </div>
-            <div class="col-lg-2 mr-2 text-center">
+            <div class="col-lg-2 mr-1 text-center">
                 <a class="d-block" href="https://twitter.com/portillo_dev" target="_blank">
                     <i class="fab fa-twitter fa-2x mb-2 text-muted"></i>
                 </a>
             </div>
-            <div class="col-lg-2 mr-2 text-center">
+            <div class="col-lg-2 mr-1 text-center">
                 <a class="d-block" href="https://www.instagram.com/portillo_dev/" target="_blank">
                     <i class="fab fa-instagram fa-2x mb-2 text-muted"></i>
                 </a>
             </div>
-            <div class="col-lg-2 mr-2 text-center">
+            <div class="col-lg-2 mr-1 text-center">
                 <a class="d-block" href="https://github.com/PortilloDev" target="_blank">
                     <i class="fab fa-github fa-2x mb-2 text-muted"></i>
                 </a>
