bd2ea39424df559116e43acca80c015c81c38741
Iván Portillo Pérez <iportillo@MacBook-Air-de-Ivan.local>
2021-12-19 09:03:06 +0100

    añadir el enlace en los titulos post

--- resources/views/components/card-post.blade.php
+++ resources/views/components/card-post.blade.php
@@ -4,8 +4,10 @@
 <div
     class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
     <div class="w-full md:w-1/3 bg-white grid place-items-center">
-        <img src="{{ Storage::url($post->image->url) }}" alt="{{ $post->name }}" title="{{ $post->name }} }}"
-            class="rounded-xl" />
+        <a href="{{ route('post', $post->slug) }}">
+            <img src="{{ Storage::url($post->image->url) }}" alt="{{ $post->name }}" title="{{ $post->name }} }}"
+                class="rounded-xl" />
+        </a>
     </div>
     <div class="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
         <div class="flex justify-between item-center gap-2">
@@ -18,7 +20,9 @@
                 @endforeach
             </p>
         </div>
-        <h3 class="font-black text-gray-800 md:text-3xl text-xl">{{ $post->name }}</h3>
+        <a href="{{ route('post', $post->slug) }}">
+            <h3 class="font-black text-gray-800 md:text-3xl text-xl">{{ $post->name }}</h3>
+        </a>
         <p class="md:text-lg text-gray-500 text-base"> {!! Str::limit($post->excerpt, 150, '...') !!}</p>
         <p class="text-xl font-black text-gray-800">
             fecha:
